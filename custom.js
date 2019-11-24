cfef57689afec2ad4bf2abe80073d9c9da1c5f3d
Benicio <benicio.daniel@gmail.com>
2019-11-24 11:10:09 -0300

    retira codigo em jQuery e atualiza pra Javascript

--- custom.js
+++ custom.js
@@ -1,23 +1,22 @@
-(function ($) {
+// media query event handler
+if (window.matchMedia("(min-width: 2200px)")) {
+    const minSize = window.matchMedia("(min-width: 2200px)");
+    minSize.addListener(WidthChange);
+    WidthChange(minSize);
+}
 
-    function mediaSize() {
-        
-        if (window.matchMedia('(min-width: 2000px)').matches) {
+// media query change
+function WidthChange(minSize) {
+    var divList = document.querySelectorAll('.about-child');
+    if (minSize.matches) {
+        divList.forEach(element => {
+            element.classList.add('col-lg-3');
+        })
+    } else {
+        divList.forEach(element => {
+            element.classList.remove('col-lg-3');
+        })
+    }
 
-            $('section > div').addClass('col-lg-3');
-
-        } else {
-
-            $('section > div').removeClass('col-lg-3');
-        }
-    };
-
-    /* Call the function */
-    mediaSize();
-    /* Attach the function to the resize event listener */
-    window.addEventListener('resize', mediaSize, false);
-
-})(jQuery);
-
-
+}
 
