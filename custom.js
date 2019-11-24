b80c17fc7e961b5906d7c2eb2383e33e31acb91a
Benicio <benicio.daniel@gmail.com>
2019-11-24 12:50:56 -0300

    cria divArchitect pra aparecer num queryMedia

--- custom.js
+++ custom.js
@@ -1,8 +1,10 @@
 // media query event handler
-if (window.matchMedia("(min-width: 2200px)")) {
-    const minSize = window.matchMedia("(min-width: 2200px)");
+if (window.matchMedia("(min-width: 2000px)")) {
+    const minSize = window.matchMedia("(min-width: 2000px)");
     minSize.addListener(WidthChange);
+    minSize.addListener(toggleDivArchitect);
     WidthChange(minSize);
+    toggleDivArchitect(minSize);
 }
 
 // media query change
@@ -12,11 +14,20 @@ function WidthChange(minSize) {
         divList.forEach(element => {
             element.classList.add('col-lg-3');
         })
+        
     } else {
         divList.forEach(element => {
             element.classList.remove('col-lg-3');
-        })
+        })        
     }
+}
 
+function toggleDivArchitect(minSize) {
+    var divArchitect = document.querySelector('.divArchitect');
+    if (minSize.matches) {
+        divArchitect.removeAttribute("hidden");
+    } else {
+        divArchitect.setAttribute("hidden", "hidden");
+    }
 }
 
